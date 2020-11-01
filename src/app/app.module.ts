6f2c99eac60e59d31f5f3f3116defce069727f8a
gabrigomezayuso <ggomezayuso@gmail.com>
2020-11-02 00:58:18 +0100

    color azul turquesa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 import { EventosComponent } from './components/eventos/eventos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    EventosComponent
+    EventosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
