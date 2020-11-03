76bcd0f5beb8cebeb901ce8d1e6efa2eecec2678
gabrigomezayuso <ggomezayuso@gmail.com>
2020-11-03 17:17:11 +0100

    nueva mejora
    solo falta introducir el checkbox

--- src/app/components/eventos/eventos.component.ts
+++ src/app/components/eventos/eventos.component.ts
@@ -26,7 +26,7 @@ export class EventosComponent implements OnInit {
   mostrarError4: string = "";
   mostrarError5: string = "";
   mostrarError6: string = "";
-  mailPatt = '^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$ ';
+  mailPatt = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
 
 
